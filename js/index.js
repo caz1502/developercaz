0d6a9ea2af40c59d37a9fa6a0639929534fdc8d8
Carol Shiers <carol.shiers@gmail.com>
2021-12-29 17:38:50 +1000

    adding files before responsive is complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// menu bar open and close
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -6,7 +7,19 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-$(document).ready(function(){
+// tool tip
+$(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+// disable more than one click on contact form
+var submitButton = document.getElementById("submit_form");
+var form = document.getElementById("email_form");
+form.addEventListener("submit", function (e) {
+  setTimeout(function () {
+    submitButton.value = "Sending...";
+    submitButton.disabled = true;
+  }, 1);
+});
+
 
